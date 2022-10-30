cbb8156ff22e8a1e328aa4fa83d831f32225c9bc
Ajay Chauhan <42740900+akchauhan2@users.noreply.github.com>
2022-10-31 00:12:19 +0530

    production url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const env = {
   baseURL: process.env.REACT_APP_ISDEV
     ? process.env.REACT_APP_BASEURL
-    : process.env.REACT_APP_BASEURL_PRODUCTION,
+    : './'
 };
 export const endPoint = {
   fetchDetails: "/music",
